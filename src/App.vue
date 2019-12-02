dd02f0559ae76d70e5f3fc764c0892fb98e6bc4d
Jack Beliaev <jackbb1972@gmail.com>
2019-12-02 21:40:03 +0300

    Second stage

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Next msg="Welcome to .... App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Next from './components/Next.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Next
   }
 }
 </script>
--- src/components/Next.vue
+++ src/components/Next.vue
@@ -1,12 +1,10 @@
 <template>
-    <div class="hello">
-        <h1>{{ msg }}</h1>
-    </div>
+    <h1>{{ msg }}</h1>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Next',
   props: {
     msg: String
   }
